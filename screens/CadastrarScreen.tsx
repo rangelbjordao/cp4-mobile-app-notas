10736ad4e33dd70072b044dfba47eea987420432
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-19 10:15:46 -0300

    base CadastrarScreen

---
+++ screens/CadastrarScreen.tsx
@@ -0,0 +1,63 @@
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
+import React, { useState } from "react";
+
+const CadastrarScreen = () => {
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
+  return (
+    <View>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+        onChangeText={setSenha}
+      />
+
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.textoBotao}>Cadastrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default CadastrarScreen;
+
+const styles = StyleSheet.create({
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    marginBottom: 12,
+    fontSize: 24,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderRadius: 10,
+    padding: 10,
+    marginBottom: 12,
+    width: 250,
+  },
+  botao: {
+    backgroundColor: "#40acff",
+    borderRadius: 10,
+    padding: 10,
+  },
+  textoBotao: {
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+});
