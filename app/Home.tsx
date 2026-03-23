11a7688c4a24a92d21539708c2217f3fe5af5457
Rangel <96200914+rangelbjordao@users.noreply.github.com>
2026-03-23 17:49:35 -0300

    funcao de logout

--- app/Home.tsx
+++ app/Home.tsx
@@ -1,20 +1,47 @@
-import { StyleSheet, Text, View } from "react-native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { router } from "expo-router";
 import React from "react";
+import { Button, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
+  const realizarLogout = async () => {
+    await AsyncStorage.removeItem("@user"); //Limpa o usuário do Async
+    router.replace("/");
+  };
   return (
-    <View>
+    <SafeAreaView style={styles.main}>
       <Text style={styles.titulo}>Home</Text>
-    </View>
+
+      <TouchableOpacity style={styles.botao} onPress={realizarLogout}>
+        <Text style={styles.textoBotao}>Sair</Text>
+      </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
 export default Home;
 
 const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
   titulo: {
     textAlign: "center",
     fontSize: 20,
+    fontWeight: "bold",
+  },
+  botao: {
+    backgroundColor: "#0a0a0a",
+    padding: 15,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  textoBotao: {
+    color: "#fafafa",
+    fontSize: 18,
     fontWeight: "bold",
   },
 });
